3ef7b1b0447444b90b85521fc362b850db0ab08b
Felipe Marrocos <marrocos@gmail.com>
2023-08-12 16:53:32 -0300

    Teste de Post Login

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,6 +1,8 @@
+import com.google.gson.Gson;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MyFirstTest {
@@ -8,8 +10,6 @@ public class MyFirstTest {
 
     @Test
     public void test01(){
-        RestAssured.baseURI = "https://serverest.dev";
-
         RestAssured.given().contentType(ContentType.JSON).log().all()
                 .when().get("usuarios")
                 .then().statusCode(HttpStatus.SC_OK).log().all();
@@ -18,7 +18,6 @@ public class MyFirstTest {
     @Test
     public void test02(){
         String id = "0uxuPY0cbmQhpEz1";
-        RestAssured.baseURI = "https://serverest.dev";
         RestAssured.given().contentType(ContentType.JSON)
                 .when().get("usuarios/{id}", id)
                 .then().statusCode(HttpStatus.SC_OK).log().all();
@@ -27,7 +26,6 @@ public class MyFirstTest {
     @Test
     public void test03(){
         String id = "Diepqtemf0017EK8";
-        RestAssured.baseURI = "https://serverest.dev";
         RestAssured.given().contentType(ContentType.JSON)
                 .when().delete("usuarios/{id}", id)
                 .then().statusCode(HttpStatus.SC_OK).log().all();
@@ -35,9 +33,21 @@ public class MyFirstTest {
 
     @Test
     public void test04(){
-        RestAssured.baseURI = "https://serverest.dev";
         RestAssured.given().contentType(ContentType.JSON)
                 .when().get("produtos")
                 .then().statusCode(HttpStatus.SC_OK).log().all();
+    }
+
+    @Test
+    public void test05(){
+                Login login = new Login("fulano@qa.com", "teste");
+        RestAssured.given().contentType(ContentType.JSON).body(login)
+                .when().post("login")
+                .then().statusCode(HttpStatus.SC_OK).log().all();
+    }
+
+    @BeforeEach
+    public void setupTest(){
+        RestAssured.baseURI = "https://serverest.dev";
     }
 }
